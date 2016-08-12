83195d14fadd7797458e6d058f9eb6174532d148
Zack Worden <zackworden@gmail.com>
2016-08-11 23:15:46 -0400

    Misc work done, but need to transfer this.

--- audioPlayer.js
+++ audioPlayer.js
@@ -1,24 +1,37 @@
 
 // const
-
+	
 // vars
 	
+// enums
+	var playModes = {
+		'normal' : 'normal',
+		'repeat' : 'repeat',
+		'shuffle' : 'shuffle',
+	};
 // classes
-	function Player( songs, elem )
+	function Player( songs, elem, playModes )
 	{
+		this.progressCallback;
+		this.progressIndicator;
 		this.playerElement = elem;
 		this.audioElement;
 		this.songs = new SongIterator( songs );
-
+		this.playMode;
 
 		// public methods
 		this.Play = function()
 		{
 			this.audioElement.play();
+			this.progressCallback = window.setInterval(this.UpdateProgressIndicator.bind( this ), 125 );
+
 			console.log('Started Playing: ' + this.songs.Current() );
 		}
 		this.Stop = function()
 		{
+			this.audioElement.pause();
+			window.clearInterval( this.progressCallback );
+			
 			console.log('Stopped Playing: ' + this.songs.Current() );
 		}
 		this.Next = function()
@@ -31,6 +44,7 @@
 			this.songs.Last();
 			this.LoadFile();
 		}
+		
 
 		// private methods
 		this.LoadFile = function()
@@ -44,8 +58,18 @@
 			this.audioElement.setAttribute('type','audio/mpeg');
 
 			this.playerElement.appendChild(this.audioElement);
+			
+			//this.progressIndicator = document.getElementById('progressBarIndicator');
 
 			this.LoadFile();
+		}
+		this.UpdateProgressIndicator = function()
+		{
+			var currentTime = this.audioElement.currentTime;
+			var maxTime = this.audioElement.duration;
+			var difference = currentTime / maxTime * 100;
+			this.progressIndicator.style.left = difference + '%';
+			console.log('progress!');
 		}
 
 		this.Init();
@@ -98,12 +122,28 @@
 		{
 			var elem = document.createElement('button');
 			elem.innerText = 'Play';
+			elem.setAttribute('id','button_play');
 			return elem;
 		}
 		this.Create_StopButton = function()
 		{
 			var elem = document.createElement('button');
 			elem.innerText = 'Stop';
+			elem.setAttribute('id','button_stop');
+			return elem;
+		}
+		this.Create_NextButton = function()
+		{
+			var elem = document.createElement('button');
+			elem.innerText = '->';
+			elem.setAttribute('id','button_next');
+			return elem;
+		}
+		this.Create_PreviousButton = function()
+		{
+			var elem = document.createElement('button');
+			elem.innerText = '<-';
+			elem.setAttribute('id','button_prev');
 			return elem;
 		}
 		this.Create_ProgressBar = function()
@@ -111,7 +151,16 @@
 			var emptyBar = document.createElement('div');
 			emptyBar.classList.add( 'progressBarBackground' );
 
-			return emptyBar;
+			var locationIndicator = document.createElement('div');
+			locationIndicator.classList.add('progressBarIndicator');
+			locationIndicator.setAttribute('id','progressBarIndicator');
+
+			var progressBar = document.createElement('div');
+			progressBar.classList.add('progressBarWrapper');
+			progressBar.appendChild(emptyBar);
+			progressBar.appendChild(locationIndicator);
+
+			return progressBar;
 		}
 
 	}
@@ -122,11 +171,26 @@
 
 		var playerElement = document.querySelector('#player');
 		var testSongs = ['audio/test1.mp3','audio/test2.mp3','audio/test3.mp3',];
-		var audioPlayer = new Player( testSongs, playerElement );
+		var audioPlayer = new Player( testSongs, playerElement, playModes );
 
 		playerElement.appendChild( playerBuilder.Create_PlayButton() );
 		playerElement.appendChild( playerBuilder.Create_StopButton() );
+		playerElement.appendChild( playerBuilder.Create_NextButton() );
+		playerElement.appendChild( playerBuilder.Create_PreviousButton() );
 		playerElement.appendChild( playerBuilder.Create_ProgressBar() );
+		audioPlayer.progressIndicator = document.getElementById('progressBarIndicator');
+
+		// temporarily assign play and stop functionality here until the html builder is finished
+		var playButton = document.querySelector('#button_play');
+		playButton.addEventListener('click', audioPlayer.Play.bind(audioPlayer) );
+		var stopButton = document.querySelector('#button_stop');
+		stopButton.addEventListener('click', audioPlayer.Stop.bind(audioPlayer) );
+		var nextButton = document.querySelector('#button_next');
+		nextButton.addEventListener('click', audioPlayer.Next.bind(audioPlayer) );
+		var prevButton = document.querySelector('#button_prev');
+		prevButton.addEventListener('click', audioPlayer.Previous.bind(audioPlayer) );
+
+//audioPlayer.Play();
 		/*
 		audioPlayer.Next();
 		audioPlayer.Play();
